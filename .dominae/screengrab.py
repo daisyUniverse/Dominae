d0f2155ba34eac2a58d4a1e3c68396f24adec95a
Elisha Shaddock <evshaddock@gmail.com>
2018-07-23 12:45:37 -0500

    Update screengrab.py
    Added -ssub, cls, screenfetch, sweet-ass, and svox

--- .dominae/screengrab.py
+++ .dominae/screengrab.py
@@ -17,6 +17,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.author.bot: return
 
     channel = message.channel
     author  = message.author
@@ -47,10 +50,22 @@ async def on_message(message):
 
     if message.content.startswith('sweb'):
 
+        await channel.send("... Snap!")
         subprocess.check_output(cwd + "/sweb.sh")
         await channel.send(file=discord.File(cwd + "/sweb.png"))
+        await channel.send("Now THAT'S going in my Cringe Compilation!")
 
         print ("Webshot Uploaded")
+        print ('by {} at {} \n'.format(author, str(datetime.now())))
+
+    if message.content.startswith('cls'):
+
+        await channel.send(file=discord.File(cwd + "/Blank.png"))
+        await channel.send(file=discord.File(cwd + "/Blank.png"))
+        await channel.send(file=discord.File(cwd + "/Blank.png"))
+        await channel.send(file=discord.File(cwd + "/Blank.png"))
+
+        print ("Chat Cleared")
         print ('by {} at {} \n'.format(author, str(datetime.now())))
 
     if message.content.startswith('smov'):
@@ -62,26 +77,71 @@ async def on_message(message):
         print ("Webm Uploaded")
         print ('by {} at {} \n'.format(author, str(datetime.now())))
 
-    if message.content.startswith('sfortune'):
+    if message.content.startswith('screenfetch'):
 
-        subprocess.check_output(cwd + "/sfortune.sh")
-        await channel.send(file=discord.File(cwd + "/sfortune.png"))
+        subprocess.check_output(cwd + "/screenfetch.sh")
+        await channel.send(file=discord.File(cwd + "/screenfetch.png"))
 
-        print ("Fortune Uploaded")
+        print ("Screenfetch Uploaded")
+        print ('by {} at {} \n'.format(author, str(datetime.now())))
+
+    if '-ass' in message.content:
+
+        ftfy = message.content.replace( "-ass " , " ass-" )
+        await channel.send("\n Fixed that for you : \n" + '```' + "{}".format(author)[:-5] + ": " + ftfy + ' ```')
+        await channel.send(file=discord.File(cwd + "/hyphen.jpg"))
+
+        print ("Sweet-Ass Uploaded")
         print ('by {} at {} \n'.format(author, str(datetime.now())))
 
     if message.content.startswith('ssay'):
 
-        message = message.content
         f = open(cwd + '/ssay.txt','w')
-        f.write(message.replace("ssay ",""))
+        f.write(message.content.replace("ssay ",""))
         f.close()
 
         subprocess.check_output(cwd + "/ssay.sh")
         await channel.send(file=discord.File(cwd + '/ssay.png'))
 
         print ("Phrase Uploaded")
-        print ("'" + message.replace("ssay ","") + "'")
+        print ("'" + message.content.replace("ssay ","") + "'")
+        print ('by {} at {} \n'.format(author, str(datetime.now())))
+
+    if message.content.startswith('svox'):
+
+        voxs = message.content.replace("svox ","")
+        voxfn = voxs.replace(" ",".wav \n") + ".wav"
+        voxfn += " svox.wav\n"
+        f = open(cwd + '/VOX/voxfn.txt','w')
+        f.write(voxfn)
+        f.close()
+
+        subprocess.check_output(cwd + "/VOX/svox.sh")
+        await channel.send(file=discord.File(cwd + '/VOX/svox.wav'))
+
+        print ("Black Mesa Resarch Facility Notified")
+        print ("'" + message.content.replace("svox ","") + "'")
+        print ('by {} at {} \n'.format(author, str(datetime.now())))
+
+    if message.content.startswith('ssub'):
+
+        f = open(cwd + '/ssub.txt','w')
+        f.write(message.content.replace("ssub ",""))
+        f.close()
+
+        subprocess.check_output(cwd + "/ssub.sh")
+        await channel.send(file=discord.File(cwd + '/ssub.png'))
+
+        print ("Subtitles Uploaded")
+        print ("'" + message.content.replace("ssub ","") + "'")
+        print ('by {} at {} \n'.format(author, str(datetime.now())))
+
+    if message.content.startswith('help svox'):
+
+        await channel.send("Here are the current VOX keywords.")
+        await channel.send(file=discord.File(cwd + "/svox.png"))
+
+        print ("Vox Keywords Shown")
         print ('by {} at {} \n'.format(author, str(datetime.now())))
 
 client.run("TOKEN")
